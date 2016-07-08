3400a1e3efab1eaf6e16636dcfe3eaa6e385b6a4
Jamess <jamessomqueiroz@gmail.com>
2016-07-08 20:56:51 -0300

    Finalizando curso

--- assets/js/adiciona.js
+++ assets/js/adiciona.js
@@ -1,5 +1,21 @@
-var pacienteNovo = "<tr class='paciente'><td class='info-nome'>Gabi</td><td class='info-peso'>74</td><td class='info-altura'>1.60</td><td class='info-imc'></td></tr>";
 
-// var tabela = document.getElementByTagName("table")[0];
-var tabela = document.querySelector("table");
-tabela.innerHTML = tabela.innerHTML + pacienteNovo
+var botao = document.querySelector( "#adicionar-paciente" );
+botao.addEventListener("click", function( event ){
+
+   // impede comportamento padrão
+   event.preventDefault();
+
+   var campoNome   = document.querySelector( "#campo-nome" );
+   var campoPeso   = document.querySelector( "#campo-peso" );
+   var campoAltura = document.querySelector( "#campo-altura" );
+
+   var pacienteNovo = "<tr class='paciente'><td class='info-nome'>"+campoNome.value+"</td><td class='info-peso'>"+campoPeso.value+"</td><td class='info-altura'>"+campoAltura.value+"</td><td class='info-imc'></td></tr>";
+
+   // var tabela = document.getElementByTagName("table")[0];
+   var tabela = document.querySelector( "table" );
+   tabela.innerHTML = tabela.innerHTML + pacienteNovo
+
+   campoPeso.value   = "";
+   campoAltura.value = "";
+   campoNome.value   = "";
+});
